905cd35d4d461b752d8ea5b0ed1d4576b820a226
(Ubuntu)Hikponss <hikponss@gmail.com>
2020-10-20 16:58:14 +0200

    translating website ...

--- js/localization/index.js
+++ js/localization/index.js
@@ -2,7 +2,7 @@ var linguJSON = {
     "languages": [
       {
           "lang_name": "English",
-          "lang_code": "en",
+          "lang_code": "us",
           "url_pattern": "?"
       },
       {
@@ -11,21 +11,36 @@ var linguJSON = {
           "url_pattern": "?"
       },
       {
-          "lang_name": "Bulgaria",
+          "lang_name": "Bulgarian",
           "lang_code": "bg",
           "url_pattern": "?"
       }
     ],
     "translated_segments": [
+        //======== HIDDEN TOP NAV BAR ========//
         {
-            "source": "Home",
-            "targer_us": "Home",
-            "target_bg": "Начало",
-            "target_de": "Startseite"
-        }
+            "source": "01Home",
+            "target_bg": "01Начало",
+            "target_de": "01Startseite"
+        },
+        {
+            "source": "Resume",
+            "target_bg": "Резюме",
+            "target_de": "Résumé"
+        },
+        //===================//
+        
+        //======== SLIDESHOW ========//
+        {
+            "source": "Hello! I'm",
+            "target_bg": "Здравейте! Аз съм",
+            "target_de": "Hallo! Ich bin"
+        },
+        //===================//
+
     ],
-    "custom_lang_switcher_container_id": "lang_Switcher_id",
-    "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a><a href=\"[[linguTargetLangHref2]]\" class=\"targetlang\">[[linguTargetLang2]]</a></div>",
+    // "custom_lang_switcher_container_id": "lang_Switcher_id",
+    // "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a><a href=\"[[linguTargetLangHref2]]\" class=\"targetlang\">[[linguTargetLang2]]</a></div>",
     
     // "translated_image_segments": [
     //     {
