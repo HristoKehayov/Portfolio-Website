8066e4f4c2f01a17367c4ca3400dec9915a469b6
(Ubuntu)Hikponss <hikponss@gmail.com>
2020-10-20 16:29:07 +0200

    translating website

---
+++ js/localization/index.js
@@ -0,0 +1,43 @@
+var linguJSON = {
+    "languages": [
+      {
+          "lang_name": "English",
+          "lang_code": "en",
+          "url_pattern": "?"
+      },
+      {
+          "lang_name": "German",
+          "lang_code": "de",
+          "url_pattern": "?"
+      },
+      {
+          "lang_name": "Bulgaria",
+          "lang_code": "bg",
+          "url_pattern": "?"
+      }
+    ],
+    "translated_segments": [
+        {
+            "source": "Home",
+            "targer_us": "Home",
+            "target_bg": "Начало",
+            "target_de": "Startseite"
+        }
+    ],
+    "custom_lang_switcher_container_id": "lang_Switcher_id",
+    "custom_lang_switcher_html": "<div id=\"langswitcher\" class=\"notranslate\"><a id=\"currlanglink\" href=\"\">[[linguCurrLang]]</a> <a href=\"[[linguTargetLangHref1]]\" class=\"targetlang\">[[linguTargetLang1]]</a><a href=\"[[linguTargetLangHref2]]\" class=\"targetlang\">[[linguTargetLang2]]</a></div>",
+    
+    // "translated_image_segments": [
+    //     {
+    //         "img_source": "http://www.lingumania.com/img/try-a-demo2.png",
+    //         "img_target_fr": "http://www.lingumania.com/img/try-a-demo-fr.png",
+    //         "img_target_es": "http://www.lingumania.com/Demos/assets/FFFFFF-0.8.png" //we don't have this image's Spanish translation, so we'll just link to a pixel image instead
+    //     }
+    // ],
+    // "translated_pages": [
+    //   {
+    //       "slug": "/Demos/Demo.html" //we only have translation for this Demo page; other links on the page (ie. link to www.lingumania.com homepage will not be prefixed with fr.lingumania.com or es.lingumania.com)
+    //   }
+    // ]
+};
+                                                 
